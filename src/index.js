22a4bee4c6fc4633e96660a3a6e96a12be0877d9
Ruslan-GH <82412111+Ruslan-GH@users.noreply.github.com>
2021-10-02 18:34:00 +0300

    add Image

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-import './sass/main.scss';
+// import './sass/main.scss';
+import menuItemTpl from './templates/menu.hbs'
+import menuList from './menu.json'
+
+const menuListContainer = document.querySelector('.js-menu');
+const menuMarkup = createMenuMarkup(menuList);
+menuListContainer.insertAdjacentHTML('beforeend', menuMarkup)
+
+function createMenuMarkup(menuList) {
+
+    return menuList.map(menuItemTpl).join('')
+}
+ 
+
+const inputThemeSwitch = document.querySelector('#theme-switch-toggle')
+
+inputThemeSwitch.addEventListener('change', (e) => {
+    if (e.target.checked) {
+        document.querySelector('body').classList.add('dark-theme')
+        localStorage.setItem('theme', 'dark-theme')
+    } else {
+        document.querySelector('body').classList.remove('dark-theme')
+        document.querySelector('body').classList.add('light-theme')
+        localStorage.setItem('theme', 'light-theme')}
+})
+
+
+
